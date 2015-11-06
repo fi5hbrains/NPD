d61b3f5337232d5b443776e35071cc29ab039f4e
fi5hbrains <bon3fish@gmail.com>
2015-11-06 17:42:13 +0300

    polish version default scope block

--- app/models/polish_version.rb
+++ app/models/polish_version.rb
@@ -1,5 +1,5 @@
 class PolishVersion < ActiveRecord::Base
-  default_scope order: 'created_at DESC'
+  default_scope {order('created_at DESC')}
 
   belongs_to :master, class_name: 'Polish', foreign_key: 'polish_id'
   serialize :polish, Hash
--- app/models/synonym.rb
+++ app/models/synonym.rb
@@ -1,4 +1,4 @@
 class Synonym < ActiveRecord::Base
   belongs_to :word, polymorphic: true
-  default_scope {order 'created_at ASC'}
+  default_scope {order( 'created_at ASC')}
 end
