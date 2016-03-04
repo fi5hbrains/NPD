2a6b835682f7e0062c4954ecbb09be3b0a419208
fi5hbrains <bon3fish@gmail.com>
2016-03-04 06:45:14 +0300

    multipart limit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(lab_icons.js profile.js lab.js amazon.js)
+
+Rack::Utils.multipart_part_limit = 512
