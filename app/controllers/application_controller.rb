8913bb5be976657f6582ea4323b863d76d8c3b43
fi5hbrains <bon3fish@gmail.com>
2016-03-06 21:27:16 +0300

    dropmenu css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   include Slugify
 
+  before_filter :no_cache_on_ajax
   helper_method :current_user_session, :current_user, :in_lab?, :coat, :set_catalogue_icons
 
   def set_users signables; User.find(signables.pluck(:user_id)) end
@@ -213,6 +214,14 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= current_user_session && current_user_session.user
+  end
+  
+  def no_cache_on_ajax
+    if request.xhr?
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+    end
   end
   
 end
