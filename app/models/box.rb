0042c918e097a53bcbb059749fc9dcf866f6629e
fi5hbrains <bon3fish@gmail.com>
2016-03-30 21:14:53 +0300

    pango debug

--- app/models/box.rb
+++ app/models/box.rb
@@ -67,11 +67,11 @@ class Box < ActiveRecord::Base
   def export_image bg = '#BBB', columns = 4
     path = Rails.root.join('public').to_s
     stack = " -background '#{bg}'"
-    self.polishes.each_with_index do |p,i|
+    (polishes = self.polishes).each_with_index do |p,i|
       stack += " \\( #{path + p.bottle_url} #{path + p.preview_url} +append -size 254x10 canvas:transparent \\( -size 454 -gravity center -background transparent  pango:\"<span  size='25000' face='PT Sans Narrow'> #{p.brand_name} \\n #{p.number} <b>#{p.name}</b></span>\" \\) -append \\) "
-      if i.modulo(columns - 1) == 0 && i > 0
-        stack += " -background '#{bg}' -alpha remove"
-        Magick.convert('', stack, "/output_#{i / columns}.png")
+      if (i.modulo(columns - 1) == 0 && i > 0) || i == (polishes.size - 1)
+        stack += " +append -background '#{bg}' -alpha remove"
+        Magick.convert('', stack, "/output_#{(i / columns).to_i}.png")
         stack = " -background '#{bg}'"
       elsif i.modulo(columns) != 0
         stack += ' +append'
