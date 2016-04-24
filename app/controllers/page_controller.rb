7fbc2695023168af0c8a4e4e503d3e79637bc3d4
fi5hbrains <bon3fish@gmail.com>
2016-04-24 11:21:05 +0300

    rescrape

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -56,20 +56,21 @@ class PageController < ApplicationController
     @result = 'wrong user, sorry'
     if current_user && current_user.name == 'bobin'
       @result = 0
-      # agent = Mechanize.new
+      agent = Mechanize.new
+      brand = Brand.find_by_slug 'orly'
+      (1..5).each do |i|
+        page = agent.get 'http://www.orlybeauty.com/nail-color/nail-color-by-types/creme.html?p=' + i.to_s
+        shades = page.search('li.item')
+        shades.each do |shade|
+          name = shade.at('.product-name').at('a').text.strip
+          polish = brand.polishes.where(name: name).first_or_create
+          if polish.draft
+            polish.remote_reference_url = shade.at('.product-image').at('img').attr('src').gsub('.jpg','.png').gsub('small_image','swapimage').gsub('_b_','_p_').gsub('lg_web','web')
+            @result += 1 if polish.save 
+          end        
+        end
+      end
       
-      agent = Mechanize.new {|a| a.ssl_version, a.verify_mode = 'TLSv1',OpenSSL::SSL::VERIFY_NONE}
-      brand = Brand.find_by_slug 'alessandro-international'
-      page = agent.get 'https://www.alessandro-international.com/b2b_uk_en/nail-polishes/colour-code-4/'
-      shades = page.search('.dot')
-      shades.each do |shade|
-        name = shade.at('.innerName').text.strip
-        polish = brand.polishes.where(name: name).first_or_create
-        if polish.draft
-          polish.remote_reference_url = shade.at('img').attr('src')
-          @result += 1 if polish.save 
-        end        
-      end      
       # ----------------------- Alessandro
       # agent = Mechanize.new {|a| a.ssl_version, a.verify_mode = 'TLSv1',OpenSSL::SSL::VERIFY_NONE}
       # brand = Brand.find_by_slug 'alessandro-international'
