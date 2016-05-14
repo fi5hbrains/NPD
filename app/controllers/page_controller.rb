59b53c9d26a6b02f9dd978b1a5f8404bddb68607
fi5hbrains <bon3fish@gmail.com>
2016-05-14 09:50:36 +0300

    slugify

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -57,20 +57,21 @@ class PageController < ApplicationController
     if current_user && current_user.name == 'bobin'
       @result = 0
       agent = Mechanize.new
+      @result = Brand.find_by_name('O·P·I').inspect
       
-      Brand.find_by_slug('opi').polishes.where("name ilike '%...%' OR name ilike '%''%'").each do |p_a|
-        p_b = Polish.find_by_slug(p_a.sulg.gsub('... ', '').gsub('...','').gsub("'",'’'))
-        if p_a.draft
-          if p_b 
-            p_b.collection = p_a.collection if p_b.collection.blank?
-            p_b.release_year = p_a.release_year if p_b.release_year.blank? || p_b.release_year == 0
-            p_b.save
-            p_a.destroy
-          end
-        else
-          #alala
-        end
-      end
+      # Brand.find_by_slug('opi').polishes.where("name ilike '%...%' OR name ilike '%''%'").each do |p_a|
+      #   p_b = Polish.find_by_slug(p_a.sulg.gsub('... ', '').gsub('...','').gsub("'",'’'))
+      #   if p_a.draft
+      #     if p_b 
+      #       p_b.collection = p_a.collection if p_b.collection.blank?
+      #       p_b.release_year = p_a.release_year if p_b.release_year.blank? || p_b.release_year == 0
+      #       p_b.save
+      #       p_a.destroy
+      #     end
+      #   else
+      #     #alala
+      #   end
+      # end
       
       # brand = Brand.find_by_slug 'astor'
       # page = agent.get  'http://www.astorcosmetics.com/products/nails/nail-color/color-care'
